0e818104722c6f9be67994a4bb793394b4d9c498
Julian Fragoso <starsmember@yahoo.com>
2023-09-21 15:48:30 -0500

    Created Pages

---
+++ src/componets/MainPagination.vue
@@ -0,0 +1,46 @@
+<script setup>
+import { ref } from 'vue' 
+const pages = ref(10)
+const activePage = ref(4)
+
+const prePage = () => {
+    if (activePage.value > 1) {
+        activePage.value--
+    }
+}
+
+const nextPage = () => {
+    if (activePage.value < pages.value) {
+        activePage.value++
+    }
+}
+
+</script>
+
+
+
+<template>
+    <div class="pageination">
+         <button class="action" :disableed="activePage === 1" @click="prePage">Prev</button>
+            <button class="page" v-for="page in pages" :class="page === activePage ? 'active' : ''" @click="activePage = page">
+                {{ page }}
+                </button>
+            <button class="action" :disabled="activePage === pages" @click="nextPage">Next</button>
+         <button> </button>
+    </div>
+</template>
+
+<style lang="postcss" scoped>
+    .pageination {
+        @apply flex justify-center gap-4;
+        .action {
+            @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200 disabled:text-slate-200 hover:disabled:bg-slate-100;
+        }
+        .page {
+            @apply rounded-md bg-slate-100 p-2 font-medium text-slate-700 shadow-md hover:bg-slate-200;
+            &.active {
+                @appply bg-yellow-700 text-slate-100 hover:bg-yellow-600;
+            }
+        }
+    }
+</style>
