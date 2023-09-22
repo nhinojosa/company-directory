4e8c42355c382e38e037c14de0fd1a8c79fed0d0
Julian Fragoso <starsmember@yahoo.com>
2023-09-22 15:32:06 -0500

    Outline is changed to blue

--- src/componets/MainSearch.vue
+++ src/componets/MainSearch.vue
@@ -29,7 +29,7 @@
         @apply relative flex w-full items-stretch;
     
         .search{
-            @apply rounded-md border-0 px-8 py-4 text-slate-700 placeholder-slate-400 outline-none focus:ring focus:ring-yellow-500;
+            @apply rounded-md border-0 px-8 py-4 text-slate-700 placeholder-slate-400 outline-none hover:focus:ring focus:ring-blue-500;
         }
         .icon {
             @apply absolute right-0 z-10 py-4 pr-8 text-slate-400;
