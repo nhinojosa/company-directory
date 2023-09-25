5c734cde5e765086b0f7f895b1e2f89df185d66e
Julian Fragoso <starsmember@yahoo.com>
2023-09-24 20:57:57 -0500

    Needed to copy for reference.

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+/* import { ref } from 'vue'
 
 const dbUsers= 
 []
@@ -20,4 +20,27 @@ export const useAuth = () => {
         }
     }
     return { isAuthenticated, user, login }
+} */
+
+import router from '@/router'
+import { firebaseApp } from '@/composables/useFirebase'
+
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'
+import { useAuth as firebaseAuth } from '@vueuse/firebase/useAuth'
+
+const auth = getAuth(firebaseApp)
+
+const { isAuthenticated, user } = firebaseAuth(auth)
+
+export const useAuth = () => {
+  const login = async (username, password) => {
+    await signInWithEmailAndPassword(auth, username, password)
+    return isAuthenticated.value
+  }
+
+  const logout = async () => {
+    await signOut(auth)
+    router.push({ name: 'Home' })
+  }
+  return { isAuthenticated, user, login, logout }
 }
