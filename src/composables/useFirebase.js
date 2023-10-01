85526594508114f006606098b93261c083806d67
Julian Fragoso <starsmember@yahoo.com>
2023-09-30 19:02:03 -0500

    Syntax Fixed

--- src/composables/useFirebase.js
+++ src/composables/useFirebase.js
@@ -4,10 +4,9 @@ const firebaseConfig = {
 apiKey: import.meta.env.VITE_FIREBASE_APIKEY,
 authDomain: import.meta.env.VITE_FIREBASE_AUTHDOMAIN,
 projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
-stroageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
-messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINSENDERID,
-appId: import.meta.env.VITE_FIREBASE_APPID
-
+storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
+messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGINGSENDERID,
+appId: import.meta.env.VITE_FIREBASE_APPID,
 }
 
 export const firebaseApp = initializeApp(firebaseConfig)
