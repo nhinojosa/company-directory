87def5eaed2809692305c5e6f440bd136efeecab
Julian Fragoso <starsmember@yahoo.com>
2023-10-04 15:11:58 -0500

    Syntax Error line 6 FIXED

--- src/components/MainCardsSingle.vue
+++ src/components/MainCardsSingle.vue
@@ -1,46 +1,29 @@
-<script setup> 
-    import { faker } from '@faker-js/faker'
-    
-    const firstName = faker.name.firstName()          
-    const lastName = faker.name.lastName()           
-    const fullName = '${firstName} ${lastName}'     
-    
+<script setup>
+  import { faker } from '@faker-js/faker'
 
-    /*Use code above instead of line 4 */
-   
-   /* 
-    const firstName =faker.name.fullName()         
-    const lastName =faker.name.lastName()           <----- DO NOT USE THIS CODE
-    const fullName = '${firstName} ${lastName}'     <----- Professor might have another way.
-    */
-    const selectCard = () => {
-     console.log('${fullName} selected')
-     }
-    </script>
+  const firstName = faker.name.firstName()
+  const lastName = faker.name.lastName()
+  const fullName = `${firstName} ${lastName}`
 
+  const selectCard = () => {
+    console.log(`${fullName} selected`)  // <----- you had '' when instead it was `` 
+  }                                      // need to be more careful on syntax errors
+</script>
 
 <template>
-
-    <div class="card" @click="selectCard">
-     <div class="card-image">
+  <div class="card" @click="selectCard">
+    <div class="card-image">
       <img :src="faker.internet.avatar()" alt="" srcset="" />
-     </div>
-    <!-- use div for divide -->
-    <!-- We did not use "div" anymore -->
-    <!-- <div> -->
-     <div class="card-details">
-       <p class="card-details-name">{{ fullName }}</p>
-       <p class="card-details-job">{{ faker.name.jobTitle() }}, {{ faker.name.jobArea() }} </p>
-       <p class="card-details-quote">"{{ faker.lorem.paragraph() }}"</p>
-     </div>
-    <!-- </div>-->
-    <!-- remember to comment out "div"-->
-    <!-- All  elements inside div are the element-->
     </div>
+    <div class="card-details">
+      <p class="card-details-name">{{ fullName }}</p>
+      <p class="card-details-job">{{ faker.name.jobTitle() }}, {{ faker.name.jobArea() }}</p>
+      <p class="card-details-quote">"{{ faker.lorem.paragraph() }}"</p>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="postcss">
-
 .card {
      
     @apply cursor-pointer overflow-hidden rounded-md bg-slate-100 p-8 shadow-md transition-transform duration-300 hover:scale-110 hover:shadow-2xl hover:shadow-blue-900;
@@ -66,5 +49,4 @@
         
     }
 }
-
 </style>
