08e73068a885460e4cb93d29cb58a2a7ac6580a3
Julian Fragoso <starsmember@yahoo.com>
2023-10-04 14:09:00 -0500

    Seperated variables and minus one space.

--- src/composables/useAuth.js
+++ src/composables/useAuth.js
@@ -1,22 +1,22 @@
 import router from '@/router'
 import { firebaseApp } from '@/composables/useFirebase'
 
-import { getAuth, signInWithEmailAndPassword, signOut } from  'firebase/auth'
+import { getAuth, signInWithEmailAndPassword, signOut } from 'firebase/auth'  // <---- from had two spaces between "from" and 'firebase/auth'
 import { useAuth as firebaseAuth } from '@vueuse/firebase/useAuth'
 
 const auth = getAuth(firebaseApp)
-const { isAuthenticated, user} = firebaseAuth(auth)
+
+const { isAuthenticated, user } = firebaseAuth(auth)   // <--- seperate all variables from the {} 
 
 export const useAuth = () => {
-    const login = async (username, password) => {
+  const login = async (username, password) => {
     await signInWithEmailAndPassword(auth, username, password)
     return isAuthenticated.value
-    }
+  }
 
-    const logout = async () => {
-        await signOut(auth)
-        router.push({ name: 'Home' })
-    }
-    return { isAuthenticated, user, login, logout}
-
+  const logout = async () => {
+    await signOut(auth)
+    router.push({ name: 'Home' })
+  }
+  return { isAuthenticated, user, login, logout }   // <--- seperate all variables from the {}
 }
