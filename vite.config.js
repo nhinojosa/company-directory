a99c254d912a7a99d2b324e705aa8bba9cfbab86
Julian Fragoso <starsmember@yahoo.com>
2023-10-04 14:11:25 -0500

    Added base and Company name.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig({ 
+  base: '/Vue-Company-Directory/',   // <---- added "base: /'Vue-Company-Directoy/" <-- don't forget the name inside single quotes
   plugins: [vue()],
   resolve:{
     alias: {
