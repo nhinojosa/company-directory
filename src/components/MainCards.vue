5e0790c40101955a92abcdf3523da70a25bfb7f4
Julian Fragoso <starsmember@yahoo.com>
2023-10-04 15:12:19 -0500

    No changes ATM

--- src/components/MainCards.vue
+++ src/components/MainCards.vue
@@ -1,19 +1,15 @@
 <script setup>
     import MainCardsSingle from '@/components/MainCardsSingle.vue'
-
 </script>
 
 <template>
     <div class="sub-wrapper">
-        <MainCardsSingle v-for="n in 20" :key="n" />
-
+      <MainCardsSingle v-for="n in 20" :key="n" />
     </div>
 </template>
 
 
 <style scoped lang="postcss">
-
-        
     .sub-wrapper {
         @apply grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4;
     }
