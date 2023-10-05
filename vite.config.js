cdd5d8c006449cff2915e6186995f55abb1c62c7
nhinojosa <nhinojosa@southtexascollege.edu>
2023-10-05 08:21:31 -0500

    fixed router

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({ 
-  base: '/Vue-Company-Directory/',   // <---- added "base: /'Vue-Company-Directoy/" <-- don't forget the name inside single quotes
+  base: '/company-directory/',   // <---- added "base: /'Vue-Company-Directoy/" <-- don't forget the name inside single quotes
   plugins: [vue()],
   resolve:{
     alias: {
