16560594823f52c881dfebd7fb1e62e9efe871c8
nhinojosa <nhinojosa@southtexascollege.edu>
2023-10-05 08:27:46 -0500

    updated router

--- src/router.js
+++ src/router.js
@@ -1,36 +1,18 @@
-/* import {createRouter, createWebHistory} from 'vue-router'
-import MainPage from '@/components/MainPage.vue'
-import LoginPage from '@/components/LoginPage.vue'
-import NotFound from '@/components/NotFound.vue'
-
-// here is the path for the MainPage and LoginPage
-// this must be renamed in order to find the path.
-const routes = [
-    { path: '/', name: 'Home', component: MainPage },
-    { path: '/login', name: 'Login', component: LoginPage },
-    { path: '/:pathMatch(.*)*', name: 'NotFound', componet: NotFound },
-  ]
-
-  export defualt createRouter({
-    history: createWebHistory(),
-    routes,
-}) */
-
 import { createRouter, createWebHistory } from 'vue-router'
-
-import { useAuth } from '@/composables/useAuth' 
-const { isAuthenticated } = useAuth()
-
+import { useAuth } from '@/composables/useAuth'
+const {isAuthenticated} = useAuth()
 import MainPage from '@/components/MainPage.vue'
 import LoginPage from '@/components/LoginPage.vue'
 import SettingsPage from '@/components/SettingsPage.vue'
 import NotFound from '@/components/NotFound.vue'
+import HomePage from '@/views/HomePage.vue'
 
 const routes = [
-  { path: '/company-directory/', name: 'Home', component: MainPage }, // <--- Change path name to "Vue-Company-Directory"
+  { path: '/vue-company-directory/', name: 'Home', component: MainPage },
   { path: '/login', name: 'Login', component: LoginPage },
-  { path: '/settings', name: 'Settings', component: SettingsPage, meta: { requiresAuth: true } },
+  { path: '/settings', name: 'Settings', component: SettingsPage, meta: {requiresAuth: true} },
   { path: '/:pathMatch(.*)*', name: 'NotFound', component: NotFound },
+  { path: '/other', name: 'Other', component: () => import('@/views/OtherPage.vue') },
 ]
 
 const router = createRouter({
